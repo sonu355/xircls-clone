bb9c32ecbfb19e157e71a34fccea967425e3f6fb
harshal <harshalc562@gmail.com>
2023-09-21 01:08:34 +0530

    done with third component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import Component1 from "./components/Component1";
 import Component2 from "./components/Component2"
+import Component3 from "./components/Component3";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Navbar/>
       <Component1 />
       <Component2 />
+      <Component3 />
     </div>
   );
 }
---
+++ src/components/Component3.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Component3 = () => {
+  return (
+    <div className='d-block mb-5 back-green px-5 text-center' style={{ padding: '30px', backgroundColor:'#8bba3b'}}>
+      <div className='container'>
+        <h3 className='three-fifth-font text-white'>
+            No Sign-up fee. No monthly commitements. &nbsp;
+            <a href='/'>
+                <span className='underline-2' style={{ color: 'white'}}>
+                    Join Here. 
+                </span>
+            </a>  
+        </h3>
+      </div>
+    </div>
+  )
+}
+
+export default Component3
