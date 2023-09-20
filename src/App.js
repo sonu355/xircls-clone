513ee2ed2fe08c008d0a1b01757db12740ad7340
harshal <harshalc562@gmail.com>
2023-09-20 18:49:29 +0530

    component no.1 is done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "./components/Navbar";
+import Component1 from "./components/Component1";
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <Component1 />
     </div>
   );
 }
---
+++ src/components/Component1.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const Component1 = () => {
+  return (
+    <>
+      <div className='back-blue my-5 py-5' style={{ backgroundColor: '#2e82cb'}}>
+        <div className='container'>
+            <div className='pb-5'>
+                <h2 className='second-font text-white text-center'>Partner With Other Buisness</h2>
+                <h5 className='sixth-font text-white text-center'>Across Sectors, Offline or Online, Anywhere in the World, Instantly.</h5>
+            </div>
+            <div className='pb-5'>
+                <h2 className='second-font text-white text-center'>Market Directly to their Customers</h2>
+                <h5 className='sixth-font text-white text-center'>At the Moment of Transaction. Without Customer Data shared.</h5>
+            </div>
+            <div className='pb-5'>
+                <h2 className='second-font text-white text-center'>Retain Existing Customers</h2>
+                <h5 className='sixth-font text-white text-center'>At Zero Cost, Via Partner Rewards.</h5>
+            </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Component1
