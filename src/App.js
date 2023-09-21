d6c9f8dcaff73ae986e90b01a39f842c63436a78
harshal <harshalc562@gmail.com>
2023-09-21 15:57:10 +0530

    component 5 done

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Component1 from "./components/Component1";
 import Component2 from "./components/Component2"
 import Component3 from "./components/Component3";
 import Component4 from "./components/Component4";
+import Component5 from "./components/Component5";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Component2 />
       <Component3 />
       <Component4 />
+      <Component5 />
     </div>
   );
 }
--- src/components/Component5.js
+++ src/components/Component5.js
@@ -1,9 +1,39 @@
 import React from 'react'
 
+const img1 = require('../asset/Com5img1.jpg')
+const img2 = require('../asset/Com5img2.jpg')
+const img3 = require('../asset/com5img3.jpg')
+const img4 = require('../asset/com5img4.jpg')
+
+
 const Component5 = () => {
   return (
-    <div>
-      
+    <div className='py-5 px-md-5 container'>
+      <h1 className='second-font text-center text-dark mt-5 mb-3'>
+      Join a Global Network of Companies
+      </h1>
+      <div className='my-card row'>
+        <div className='py-2 text-center col-md-3'>
+          <img src={img1} width='64' alt='img-fluid'></img>
+          <h4 className='text-blue mt-2 mb-1 fourth-font' style={{ color:'#2e82cb'}}>DECENTRALIZED</h4>
+          <h5 className='fifth-font lh-sm'>Global network of companies with no third-party or central authority.</h5>
+        </div>
+        <div className='py-2 text-center col-md-3'>
+          <img src={img2} width='64' alt='img-fluid'></img>
+          <h4 className='text-blue mt-2 mb-1 fourth-font' style={{ color:'#2e82cb'}}>PEER-TO-PEER VERIFIED</h4>
+          <h5 className='fifth-font lh-sm'>Real-time customer verification by companies.</h5>
+        </div>
+        <div className='py-2 text-center col-md-3'>
+          <img src={img3} width='64' alt='img-fluid' ></img>
+          <h4 className='text-blue mt-2 mb-1 fourth-font' style={{ color:'#2e82cb'}}>LEDGER-BASED</h4>
+          <h5 className='fifth-font lh-sm'>A ledger-based verification model that encompasses transactions and actions.</h5>
+        </div>
+        <div className='py-2 text-center col-md-3'>
+          <img src={img4} width='64' alt='img-fluid'></img>
+          <h4 className='text-blue mt-2 mb-1 fourth-font' style={{ color:'#2e82cb'}}>NO DATA SHARING</h4>
+          <h5 className='fifth-font lh-sm'>Customer data remains localized and 100% secure with built-in data protocols.</h5>
+        </div>
+      </div>
     </div>
   )
 }
